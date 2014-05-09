c2e9eec92eab9d8f91b4a6eea3c599b6263f53b7
Ajico KIRIHARA <orangepillow@gmail.com>
2014-05-09 22:01:07 +0900

    Add feature spec

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -5,6 +5,16 @@ feature KayochinGohan::App do
   scenario 'httpまたはhttps以外のURLを入力する' do
     visit '/'
     fill_in 'image_url', with: 'javascript://example.com'
+    click_button 'image_url_submit'
     expect(page).to have_content('kayochin gohan')
+  end
+
+  scenario 'リソースが存在しないURLを入力する' do
+    visit '/'
+    fill_in 'image_url', with: 'http://app.naidente.org/notfound.jpg'
+    click_button 'image_url_submit'
+    expect(page.status_code).to eq 404
+    expect(page).to have_content('error')
+    expect(page).to have_content('指定したURLの画像は存在しません')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,9 +13,8 @@ require 'rack/test'
 ENV['RACK_ENV'] = 'test'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.treat_symbols_as_metadata_keys_with_true_values = false
+  config.run_all_when_everything_filtered = false
 
   config.include Rack::Test::Methods
   config.include Capybara::DSL
