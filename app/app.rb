f1e755b98c38c0a4a684d8b7313b920a795fa179
Ajico KIRIHARA <orangepillow@gmail.com>
2014-05-15 03:29:06 +0900

    Extract image_filter.rb

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,8 @@ Bundler.require
 require 'digest/md5'
 require 'rack-flash'
 
+require File.join(File.dirname(__FILE__), 'image_filter.rb')
+
 module KayochinGohan
   PUBLIC_ROOT = 'app/public'
 
@@ -195,90 +197,5 @@ module Character
     def exist?
       File.exist?(image_path)
     end
-  end
-end
-
-class ImageFilter
-  FILTERS = %w(none grayscale sepia toaster gotham lomo kelvin)
-
-  def self.apply(image, name)
-    if self.exist?(name)
-      image = send(filter_method(name), image)
-    else
-      image
-    end
-  end
-
-  def self.exist?(name)
-    self.methods.map{ |m| m.to_s }.include?(filter_method(name))
-  end
-
-  def self.filter_method(name)
-    "#{name}_filter"
-  end
-
-  def self.grayscale_filter(image)
-    image.colorspace('Gray')
-    image
-  end
-
-  def self.sepia_filter(image)
-    image.sepia_tone '80%'
-    image
-  end
-
-  def self.toaster_filter(image)
-    new_image = image.clone
-    new_image.combine_options do |cmd|
-      cmd.fill '#330000'
-      cmd.colorize '63%'
-    end
-    image = image.composite new_image do |cmd|
-      cmd.compose 'blend'
-      cmd.define 'compose:args=100,0'
-    end
-
-    image.modulate '150,80,100'
-    image.gamma 1.2
-    image.contrast
-    image.contrast
-    image
-  end
-
-  def self.gotham_filter(image)
-    image.modulate '120,10,100'
-    image.fill '#222b6d'
-    image.colorize 20
-    image.gamma 0.5
-    image.contrast
-    image
-  end
-
-  def self.lomo_filter(image)
-    image.channel 'R'
-    image.level '22%'
-    image.channel 'G'
-    image.level '22%'
-    image
-  end
-
-  def self.kelvin_filter(image)
-    cols, rows = image[:dimensions]
-
-    image.auto_gamma
-    image.modulate '120,50,100'
-
-    new_image = image.clone
-    new_image.combine_options do |c|
-      c.fill 'rgba(255, 153, 0, 0.5)'
-      c.draw "rectangle 0,0 #{cols},#{rows}"
-    end
-
-    image = image.composite new_image do |c|
-      c.compose 'multiply'
-    end
-
-    image.gamma 1.2
-    image
   end
 end
